0d6632b446da972552825e009d4add839f5af95e
Abhishek <abhishek@example.com>
2026-01-03 17:00:38 +0530

    Updated backend URLs to deployed API

--- src/Pages/Mostexpectedvote.jsx
+++ src/Pages/Mostexpectedvote.jsx
@@ -1,26 +1,25 @@
 import React, { useEffect, useState } from "react";
 import "./Mostexpectedvote.css";
+import { API_URL } from "../config";
 
 const Mostexpectedvote = () => {
   const [topStudents, setTopStudents] = useState([]);
 
-  const loadTopStudents = async () => {
-    const res = await fetch("http://localhost:5000/students/top");
-    const data = await res.json();
-    setTopStudents(data);
-  };
-
   useEffect(() => {
-    loadTopStudents();
+    fetch(`${API_URL}/students/top`)
+      .then(res => res.json())
+      .then(data => setTopStudents(data))
+      .catch(console.error);
   }, []);
 
   return (
     <div className="Mostexpectedvotemaindiv">
       <h1>Top 5 Most Voted Students</h1>
+
       <div className="Mostexpectedvotediv">
-        {topStudents.map((s) => (
+        {topStudents.map(s => (
           <div className="Mostexpectedvote" key={s._id}>
-            <img src={s.Imgsrc} alt={s.name} width="50px" />
+            <img src={`${API_URL}${s.Imgsrc}`} alt={s.name} width="60" />
             <p>Name: {s.name}</p>
             <p>Total Votes: {s.votes}</p>
           </div>
