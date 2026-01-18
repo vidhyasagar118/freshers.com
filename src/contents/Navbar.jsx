f47da354e628ebf4a0cbc68e74d95ef4ec749e5a
Abhishek <abhishek@example.com>
2026-01-18 11:05:55 +0530

    abhishekh

--- src/contents/Navbar.jsx
+++ src/contents/Navbar.jsx
@@ -44,7 +44,7 @@ const Navbar = () => {
           </li>
           <li>
             {user ? (
-              <div className="userlogin" onClick={() => navigate("/")}>
+              <div className="userlogin" onClick={() => navigate("/login")}>
                 <img
                   src="https://as1.ftcdn.net/v2/jpg/02/01/33/54/1000_F_201335438_CNpY0iWaXXAV95Gj8BPB0tEJlMcxWeaZ.jpg" width="40px"
                   alt={user.name}
